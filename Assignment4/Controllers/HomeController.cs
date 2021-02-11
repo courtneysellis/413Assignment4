7cf4a3ebc0e5401ecdb8522c32d1b2ea93de5758
Courtney Ellis <courtneycaloca@gmail.com>
2021-02-10 22:09:08 -0700

    FInal

--- Assignment4/Controllers/HomeController.cs
+++ Assignment4/Controllers/HomeController.cs
@@ -20,13 +20,63 @@ namespace Assignment4.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<string> restaurantList = new List<string>();
+
+            foreach (Restaurant r in Restaurant.GetRestaurants())
+            {
+                if (r.FavDish == null)
+                {
+                    r.FavDish = "It's all tasty!";
+                }
+
+                if (r.Website == "Website coming soon!")
+                {
+                    restaurantList.Add($"#{r.Rank}: {r.Name}<br>Favorite Dish: {r.FavDish}<br>{r.Address}<br>{r.Phone}<br>{r.Website}");
+                }
+                else
+                {
+                    restaurantList.Add($"#{r.Rank}: {r.Name}<br>Favorite Dish: {r.FavDish}<br>{r.Address}<br>{r.Phone}<br><a href=\"{r.Website}\">{r.Website}</a>");
+                }
+            }
+
+            return View(restaurantList);
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult RestaurantForm()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult RestaurantForm(UserRestaurant userRest)
+        {
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddRestaurantToList(userRest);
+                return View("Confirm", userRest);
+            }
+
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public IActionResult RestaurantList()
+        {
+            foreach (UserRestaurant r in TempStorage.userRestaurantList)
+            {
+                if (r.FavDish == null)
+                {
+                    r.FavDish = "It's all tasty!";
+                }
+            }
+
+            return View(TempStorage.userRestaurantList);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
